d4f8b705fdc53e226e65dadb4b4c5a1f6d033350
Nemanja Jeremic <nemanja@joker.rs>
2019-06-21 19:30:36 +0200

    napravljena klasicna login forma, sa php delom koji omogucava rad forme, koji se poziva na klasu Users

--- login.php
+++ login.php
@@ -1,17 +1,87 @@
 <?php
-	
+    // Pocetak sesije
+    session_start();
 	// Konekcija sa bazom podataka (MySql)
     require_once ("./konfiguracija.php");
     // Klasa korisnici.php
     require_once ("./klase/korisnici.php");
-    // Pocetak sesije
-    session_start();
 
-
-    $test = new Users($conn);
-    $test1 = $test->getUser(1);
-    foreach ($test1 as $test12) {
-    	echo "<pre>";
-    	print_r($test12);
-    	echo "</pre>";
+    if(isset($_POST['username'], $_POST['password'])){
+    	$UserLogin = new Users($conn);
+    	$UserLogin_bool = $UserLogin->userLogin($_POST['username'], $_POST['password']);
+    	if($UserLogin_bool){
+    		$getUserDataByUserName = new Users($conn);
+    		$userData_byName = $getUserDataByUserName->getUserName($_POST['username']);
+    		foreach ($userData_byName as $Userdata) {
+    			$_SESSION['uid'] = $Userdata['id'];
+    			header('Location: admin.php');
+    		}
+    	}
     }
+    if(isset($_SESSION['uid'])){
+    			header('Location: admin.php');;
+    }
+?>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="css/login.css">
+
+    <link rel="icon" href="Favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <title>Login | Zadatak 1</title>
+</head>
+<body>
+<main class="login-form">
+    <div class="cotainer">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-header">Login</div>
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            <div class="form-group row">
+                                <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
+                                <div class="col-md-6">
+                                    <input type="text" id="username" class="form-control" name="username" required autofocus>
+                                </div>
+                            </div>
+                            <?php print_r($_SESSION); ?>
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                                <div class="col-md-6">
+                                    <input type="password" id="password" class="form-control" name="password" required>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Login
+                                </button>
+                            </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</main>
+</body>
+</html>
