2a3716ab852f7bca50544a577906baaca29628df
Nemanja Jeremic <nemanja@joker.rs>
2019-06-21 15:49:33 +0200

    Test sa bazom

--- konfiguracija.php
+++ konfiguracija.php
@@ -3,4 +3,19 @@
 $password = "Nemanjabg44";
 $username = "root";
 $dbhost = "localhost";
-$dbname = "";
+$dbname = "zadatak1";
+
+	try { // Konekcija za bazom PDO
+		    $conn = new PDO('mysql:host='.$dbhost.';dbname='.$dbname, $username, $password);
+		    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $e) {
+		    print "Error!: " . $e->getMessage() . "<br/>";
+		    die();
+		}	
+
+
+		$test = $conn->prepare("SELECT * FROM users");
+		$test->execute();
+		foreach ($test as $test1) {
+			print_r($test1);
+		}
