17720bc8e3b0445da5fc6fcd9cb0e28f8af9d82d
Nemanja Jeremic <nemanja@joker.rs>
2019-06-25 19:42:27 +0200

    upload slike

---
+++ upload.php
@@ -0,0 +1,11 @@
+<?php
+if($_FILES["file"]["name"] != '')
+{
+ $test = explode('.', $_FILES["file"]["name"]);
+ $ext = end($test);
+ $name =md5(rand(100, 999)) . '.' . $ext;
+ $location = 'images/' . $name;  
+ move_uploaded_file($_FILES["file"]["tmp_name"], $location);
+ echo $location;
+}
+?>
