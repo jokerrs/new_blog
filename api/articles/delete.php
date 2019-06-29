d46f82a32a033d2cd3350ad0bc868b4ed6ec9347
Nemanja Jeremic <nemanja@joker.rs>
2019-06-29 14:14:57 +0200

    formatiranje koda

--- api/articles/delete.php
+++ api/articles/delete.php
@@ -11,21 +11,20 @@ if(isset($_SESSION['uid'])){
 	$author_id = $_SESSION['uid'];
 	$data = json_decode(file_get_contents("php://input"));
 	$delete = $data->id;
-			$objave = new Articles($conn);
+	$objave = new Articles($conn);
 	if($objave->getIsAuthor($author_id, $delete)){
 		if(!empty($data->id)){
 			if($objave->deleteArticle($delete)){
 				http_response_code(200);
-			}else{
-			    http_response_code(503);
-			    }
-			}else{
-				http_response_code(400);
-			}
+				}else{
+				    http_response_code(503);
+				}
+		}else{
+			http_response_code(400);
+		}
 	}else{
 		http_response_code(403.3);
 	}
-
 }else{
 		http_response_code(403.3);
 }
