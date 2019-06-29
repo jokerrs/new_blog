96ef72199e8748201c0a2435a9ce83a40aa1f8c3
Nemanja Jeremic <nemanja@joker.rs>
2019-06-29 14:16:04 +0200

    brisanje iz navigacije linkova za delete i izmenu

--- edit.php
+++ edit.php
@@ -54,8 +54,6 @@
           <li class="nav-item"><a class="nav-link" href="<?= $link_sajta."admin.php"; ?>">Pocetna</a></li>
           <li class="nav-item"><a class="nav-link" href="<?= $link_sajta."post_list.php"; ?>">Lista postova</a></li>
           <li class="nav-item"><a class="nav-link" href="<?= $link_sajta."add_new.php"; ?>">Dodaj novi post</a></li>
-          <li class="nav-item active" active><a class="nav-link" href="<?= $link_sajta."edit.php"; ?>">Izmeni post</a></li>
-          <li class="nav-item"><a class="nav-link" href="<?= $link_sajta."delete.php"; ?>">Obrisi post</a></li>
           <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
         </ul>
       </div>
