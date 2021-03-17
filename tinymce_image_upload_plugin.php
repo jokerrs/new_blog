39bde5d4f90e10b4b1f171b60d99d4c29fbbc8b3
jokerrs <Nemanjabg44!>
2021-03-17 06:06:59 +0100

    rand => mt_rand

--- tinymce_image_upload_plugin.php
+++ tinymce_image_upload_plugin.php
@@ -3,7 +3,7 @@ if($_FILES["file"]["name"] != '')
 {
  $test = explode('.', $_FILES["file"]["name"]);
  $ext = end($test);
- $name =md5(rand(100, 999)) . '.' . $ext;
+ $name =md5(mt_rand(100, 999)) . '.' . $ext;
  $location = 'images/' . $name;  
  move_uploaded_file($_FILES["file"]["tmp_name"], $location);
  echo json_encode(array(
